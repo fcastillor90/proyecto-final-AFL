355f0a0f58c345e5165a88bacecbf6b9bc836ad2
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-11-20 14:48:35 +0000

    no cambia color

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -8,14 +8,6 @@ export const Navbar = () => {
 	return (
 		<nav className="navbar" style={{ backgroundColor: 'black' }}>
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1"><img src={navbarBrandImg} style={{ width: '130px' }} /></span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
 				<PrimarySearchAppBar />
 			</div>
 		</nav>
