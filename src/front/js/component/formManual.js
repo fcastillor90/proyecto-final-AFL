c008a9bc426653a753541f8fb0a16900d86b50bd
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-11-20 20:35:17 +0000

    termine formas

--- src/front/js/component/formManual.js
+++ src/front/js/component/formManual.js
@@ -4,7 +4,39 @@ import { Link } from 'react-router-dom';
 export const FormManual = () => {
     return (
         <div>
-            <p>form manual</p>
+
+
+<form className="mx-auto w-25 mt-5 p-5" style={{ border: '1px solid #ddd', borderRadius: '20px' }}>
+                <div className="mb-3">
+                    <label htmlFor="inputNombreEmpresa" className="form-label">Nombre empresa</label>
+                    <input type="text" className="form-control" id="inputNombreEmpresa" />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Ciudad</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Nombre Rol</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Categoría del Rol</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Años de experiencia</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+
+
+                <button type="submit" className="btn" style={{ backgroundColor: '#4f89ee' }}>Submit</button>
+            </form>
+
+
             <div className='text-center mt-5'>
                 <Link to="/formOptions">
                     <p>Volver a pagina anterior</p>
