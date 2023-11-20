68f2296921d0b6e3466eff33c8f1564f61daa4af
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-11-20 14:10:09 +0000

    navbar de mui instalado, falta editarlo

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import navbarBrandImg from "../../img/navbar-brand-img.png";
+import PrimarySearchAppBar from "../component/primarySearchAppBar.js";
+
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar" style={{ backgroundColor: 'black' }}>
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1"><img src={navbarBrandImg} style={{ width: '130px' }} /></span>
 				</Link>
 				<div className="ml-auto">
 					<Link to="/demo">
 						<button className="btn btn-primary">Check the Context in action</button>
 					</Link>
 				</div>
+				<PrimarySearchAppBar />
 			</div>
 		</nav>
 	);
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-
+//MUI:
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
@@ -37,64 +37,68 @@ export const Home = () => {
 };
 
 
+
+
 function CustomTabPanel(props) {
 	const { children, value, index, ...other } = props;
-  
+
 	return (
-	  <div
-		role="tabpanel"
-		hidden={value !== index}
-		id={`simple-tabpanel-${index}`}
-		aria-labelledby={`simple-tab-${index}`}
-		{...other}
-	  >
-		{value === index && (
-		  <Box sx={{ p: 3 }}>
-			<Typography>{children}</Typography>
-		  </Box>
-		)}
-	  </div>
+		<div
+			role="tabpanel"
+			hidden={value !== index}
+			id={`simple-tabpanel-${index}`}
+			aria-labelledby={`simple-tab-${index}`}
+			{...other}
+		>
+			{value === index && (
+				<Box sx={{ p: 3 }}>
+					<Typography>{children}</Typography>
+				</Box>
+			)}
+		</div>
 	);
-  }
-  
-  CustomTabPanel.propTypes = {
+}
+
+CustomTabPanel.propTypes = {
 	children: PropTypes.node,
 	index: PropTypes.number.isRequired,
 	value: PropTypes.number.isRequired,
-  };
-  
-  function a11yProps(index) {
+};
+
+function a11yProps(index) {
 	return {
-	  id: `simple-tab-${index}`,
-	  'aria-controls': `simple-tabpanel-${index}`,
+		id: `simple-tab-${index}`,
+		'aria-controls': `simple-tabpanel-${index}`,
 	};
-  }
-  
-  export default function BasicTabs() {
+}
+
+export default function BasicTabs() {
 	const [value, setValue] = React.useState(0);
-  
+
 	const handleChange = (event, newValue) => {
-	  setValue(newValue);
+		setValue(newValue);
 	};
-  
+
 	return (
-	  <Box sx={{ width: '100%' }}>
-		<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-		  <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-			<Tab label="Item One" {...a11yProps(0)} />
-			<Tab label="Item Two" {...a11yProps(1)} />
-			<Tab label="Item Three" {...a11yProps(2)} />
-		  </Tabs>
+		<div className="ms-5">
+		<Box sx={{ width: '100%', margin: 'auto' }}>
+			<Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+				<Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+					<Tab label="Item One" {...a11yProps(0)} />
+					<Tab label="Item Two" {...a11yProps(1)} />
+					<Tab label="Item Three" {...a11yProps(2)} />
+				</Tabs>
+			</Box>
+			<CustomTabPanel value={value} index={0}>
+				Item One
+			</CustomTabPanel>
+			<CustomTabPanel value={value} index={1}>
+				Item Two
+			</CustomTabPanel>
+			<CustomTabPanel value={value} index={2}>
+				Item Three
+			</CustomTabPanel>
 		</Box>
-		<CustomTabPanel value={value} index={0}>
-		  Item One
-		</CustomTabPanel>
-		<CustomTabPanel value={value} index={1}>
-		  Item Two
-		</CustomTabPanel>
-		<CustomTabPanel value={value} index={2}>
-		  Item Three
-		</CustomTabPanel>
-	  </Box>
+		</div>
 	);
-  }
+}
