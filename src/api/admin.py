da09a7ea90e67db48c075a207ea74ef91f08c964
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-11-25 22:10:04 +0000

    updated models and admin

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User, Login, Movies, Watchlist, Movie_Rating, Comments, Likes, Dislikes
+from .models import db, User, Login, Category, Role, Salary
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -13,12 +13,9 @@ def setup_admin(app):
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Login, db.session))
-    admin.add_view(ModelView(Movies, db.session))
-    admin.add_view(ModelView(Watchlist, db.session))
-    admin.add_view(ModelView(Movie_Rating, db.session))
-    admin.add_view(ModelView(Comments, db.session))
-    admin.add_view(ModelView(Likes, db.session))
-    admin.add_view(ModelView(Dislikes, db.session))
+    admin.add_view(ModelView(Category, db.session))
+    admin.add_view(ModelView(Role, db.session))
+    admin.add_view(ModelView(Salary, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -3,18 +3,13 @@ db = SQLAlchemy()
 
 
 class User(db.Model):
-    __tablename__='user'
+    __tablename__='users'
     id = db.Column(db.Integer, primary_key=True)
     username=db.Column(db.String(120), unique=True, nullable=False)
     name=db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    watchlist = db.relationship('Watchlist', backref='user', lazy=True)
-    movie_rating = db.relationship('Movie_Rating', backref='user', lazy=True)
-    comments = db.relationship('Comments', backref='user', lazy=True)
-    likes = db.relationship('Likes', backref='user', lazy=True)
-    dislikes = db.relationship('Dislikes', backref='user', lazy=True)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -42,124 +37,57 @@ class Login(db.Model):
             " is_active ":self. is_active
             # do not serialize the password, its a security breach
         }
-    
-class Movies(db.Model):
-    __tablename__='movies'
+
+class Category(db.Model):
+    __tablename__='category'
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), unique=True, nullable=False)
-    genre = db.Column(db.String(120), nullable=True)
-    rating = db.Column(db.String(120), nullable=False)
-    year = db.Column(db.Integer, nullable=True)
-    budget = db.Column(db.String(120), nullable=True)
-    description = db.Column(db.String(120), nullable=True)
-    tagline = db.Column(db.String(120), nullable=True)
-    image = db.Column(db.String(500), nullable=False)
-    watchlist = db.relationship('Watchlist', backref='movies', lazy=True)
-    movie_rating = db.relationship('Movie_Rating', backref='movies', lazy=True)
-    comments = db.relationship('Comments', backref='movies', lazy=True)
+    name = db.Column(db.String(120), nullable=False)
+
+    roles = db.relationship('Role', backref='category', lazy=True)
 
     def __repr__(self):
-        return f'<Movies {self.title}>'
+        return f'<Category {self.id} {self.name}>'
 
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name
+        }
+
+    
+class Role(db.Model):
+    __tablename__='roles'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False) # Specify the foreign key relationship
+
+    def __repr__(self):
+        return f'<Role {self.id}>'
+    
     def serialize(self):
         return {
             "id": self.id,
             "title": self.title,
-            "genre": self.genre,
-            "rating": self.rating,
-            "year": self.year,
-            "budget": self.budget,
-            "description": self.description,
-            "tagline": self.tagline,
-            "image": self.image
+            "category_id": self.category_id
         }
 
-class Watchlist(db.Model):
-    __tablename__='watchlist'
+
+class Salary(db.Model):
+    __tablename__='salary'
     id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    years_of_experience = db.Column(db.Integer, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    country = db.Column(db.String(120), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
 
     def __repr__(self):
-        return f'<Watchlist {self.id} {self.movie_id}>'
+        return f'<Salary {self.id} {self.amount}>'
 
     def serialize(self):
         return {
             "id": self.id,
-            "author_id": self.author_id,
-            "movie_id": self.movie_id
-        }
-
-class Movie_Rating(db.Model):
-    __tablename__='movie_rating'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-
-    def __repr__(self):
-        return f'<MovieRating {self.rating}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "movie_id": self.movie_id,
-            "rating": self.rating
-        }
-
-class Comments(db.Model):
-    __tablename__='comments'
-    id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable=False)
-    content = db.Column(db.String(3000), nullable=False)
-    like_total = db.Column(db.Integer, nullable=False)
-    dislike_total = db.Column(db.Integer, nullable=True)
-    likes = db.relationship('Likes', backref='comments', lazy=True)
-    dislikes = db.relationship('Dislikes', backref='comments', lazy=True)
-
-    def __repr__(self):
-        return f'<Comments {self.id}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "author_id": self.author_id,
-            "movie_id": self.movie_id,
-            "content": self.content,
-            "like_total": self.like_total,
-            "dislike_total": self.dislike_total
+            "amount": self.amount,
+            "city" : self.city
         }
     
-class Likes(db.Model):
-    __tablename__='likes'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'), nullable=False)
-
-    def __repr__(self):
-        return f'<Likes {self.id}>'
-    
-    def serialize(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "comment_id": self.comment_id
-        }
-    
-class Dislikes(db.Model):
-    __tablename__='dislikes'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    comment_id = db.Column(db.Integer, db.ForeignKey('comments.id'), nullable=False)
-
-    def __repr__(self):
-        return f'<Dislikes {self.id}>'
-    
-    def serialize(self):
-        return {
-            "id": self.id,
-            "user_id": self.user_id,
-            "comment_id": self.comment_id
-        }
